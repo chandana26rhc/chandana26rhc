946a6634ff32acf94306d42748d1923f81068398
chandana26rhc <92939185+chandana26rhc@users.noreply.github.com>
2021-10-25 12:17:49 +0530

    Add files via upload

---
+++ marks.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+
+	int phy,che,mat,eng,tot;
+	
+	phy=34;
+	che=38;
+	mat=28;
+	eng=39;
+	tot=phy+che+mat+eng;
+	printf("marks in physics:%d\n",phy);
+	printf("marks in chemistry:%d\n",che);
+	printf("marks in maths:%d\n",mat);
+	printf("marks in english:%d\n",eng);
+	printf("total marks:%d",tot);
+    
+	}
